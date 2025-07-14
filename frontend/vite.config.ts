e90089840fc91a8fc7cd26a5ca0705539f0a56f7
amanmishra75way <amishra.75way@gmail.com>
2025-07-14 12:58:59 +0530

    Basic

---
+++ frontend/vite.config.ts
@@ -0,0 +1,19 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    svgr({
+      svgrOptions: {
+        exportType: "named",
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: "**/*.svg",
+    }),
+    react(),
+  ],
+});
